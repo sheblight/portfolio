b215c19781bd08de2007009b9fb598bd38fc1a84
sheblight <sheblight@gmail.com>
2023-02-14 17:52:52 -0500

    Add mouse hover color change on nav tab

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -2,37 +2,32 @@ import './App.css';
 
 function SideMenu() {
     function NavTab(prop) {
+        const onHover = function(isHovering){ 
+            return function(e) {
+                e.target.style.backgroundColor = isHovering ? "rgba(100,100,100,0.5)" : "transparent";
+                e.target.style.color = isHovering ? "var(--text-color)" : "var(--text-color)";
+            }
+        };
+
         return (
-            <li>{prop.name}</li>
+            <li onMouseEnter={onHover(true)} onMouseLeave={onHover(false)} style={{margin:"0.5rem 0"}}>{prop.name}</li>
         );
     }
 
-    const flexStyle = { 
-        display: "flex", 
-        flexDirection: "column", 
-        justifyContent: "space-between", 
-        alignItems: "flex-end", 
-        padding: "25px", 
-        borderRight: "5px dashed var(--text-color)", 
-        background: "var(--menu-color)", 
-        textAlign: "right", 
-        fontSize: "25px", 
-        color: "var(--text-color)" 
-    };
     const gridStyle = { 
         display: "grid",
         gridTemplateRows: "4rem auto 3rem 10rem",
-        padding: "25px",
+        padding: "1.5rem",
         borderRight: "5px dashed var(--text-color)", 
         background: "var(--menu-color)", 
         textAlign: "right", 
-        fontSize: "25px", 
+        fontSize: "1.5rem", 
         color: "var(--text-color)" 
     };
 
     return (
         <nav className="side-menu" style={gridStyle}>
-            <h1 style={{ marginBottom: "35px", fontFamily: "var(--header-font)", textAlign: "right", fontSize: "30px", fontWeight: "bold", textTransform: "none" }}>Hibiki Takaku</h1>
+            <h1 style={{ marginBottom: "2rem", fontFamily: "var(--header-font)", textAlign: "right", fontSize: "2rem", fontWeight: "bold", textTransform: "none" }}>Hibiki Takaku</h1>
             <ul className="nav-tabs">
                 <NavTab name="Sites"/>
                 <NavTab name="Games"/>
@@ -41,8 +36,8 @@ function SideMenu() {
             </ul>
             <div className="divider"></div>
             <footer style={{width: "230px", color: "var(--text-color)"}}>
-                <a href="mailto:sheblight@gmail.com" style={{fontFamily: "var(--header-font)", textDecoration: "none", color: "var(--text-color)", letterSpacing: "1px", fontSize: "15px"}}>sheblight@gmail.com</a>
-                <div className="external-icons" style={{display: "flex", height: "50px", justifyContent: "flex-end", alignItems: "center", gap: "10px"}}>
+                <a href="mailto:sheblight@gmail.com" style={{fontFamily: "var(--header-font)", textDecoration: "none", color: "var(--text-color)", letterSpacing: "1px", fontSize: "1rem"}}>sheblight@gmail.com</a>
+                <div className="external-icons" style={{display: "flex", height: "50px", justifyContent: "flex-end", alignItems: "center", gap: "1rem"}}>
                     <a target="_blank" rel="noreferrer" href="https://soundcloud.com/hibiki-takaku">
                     <svg className="soundcloud" style={{width:"22px", height:"22px", padding:"4px"}} viewBox="0 0 24 24">
                         <path fill="currentColor" d="M11.56,8.87V17H20.32V17C22.17,16.87 23,15.73 23,14.33C23,12.85 21.88,11.66 20.38,11.66C20,11.66 19.68,11.74 19.35,11.88C19.11,9.54 17.12,7.71 14.67,7.71C13.5,7.71 12.39,8.15 11.56,8.87M10.68,9.89C10.38,9.71 10.06,9.57 9.71,9.5V17H11.1V9.34C10.95,9.5 10.81,9.7 10.68,9.89M8.33,9.35V17H9.25V9.38C9.06,9.35 8.87,9.34 8.67,9.34C8.55,9.34 8.44,9.34 8.33,9.35M6.5,10V17H7.41V9.54C7.08,9.65 6.77,9.81 6.5,10M4.83,12.5C4.77,12.5 4.71,12.44 4.64,12.41V17H5.56V10.86C5.19,11.34 4.94,11.91 4.83,12.5M2.79,12.22V16.91C3,16.97 3.24,17 3.5,17H3.72V12.14C3.64,12.13 3.56,12.12 3.5,12.12C3.24,12.12 3,12.16 2.79,12.22M1,14.56C1,15.31 1.34,15.97 1.87,16.42V12.71C1.34,13.15 1,13.82 1,14.56Z" />
@@ -54,7 +49,7 @@ function SideMenu() {
                     </svg>
                     </a>
                 </div>
-                <p style={{fontFamily: "var(--header-font)", fontSize: "17px"}}>© 2023 Hibiki Takaku</p>
+                <p style={{fontFamily: "var(--header-font)", fontSize: "1rem"}}>© 2023 Hibiki Takaku</p>
             </footer>
         </nav>
     );
