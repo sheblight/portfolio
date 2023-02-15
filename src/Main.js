c27ef854e04d129b29fd7a3b5c0c1a41d0608fc9
sheblight <sheblight@gmail.com>
2023-02-15 00:20:05 -0500

    Add placeholder entries for projects

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,26 @@
 import './App.css';
 
-function SiteEntry(props) {
+function ProjectEntry(props) {
+    const content = props.content;
+    const embedVideo = (<div></div>);
+    const screenshot = (src, alt) => <img className="thumbnail" src={require(`${src}`)} alt={alt}/>;
     return (
-        <div>
-            <h2>Entry Title</h2>
-            <p>Description</p>
+        <div className="project-entry">
+            <h2>{content.title}</h2>
+            <div className="divider"></div>
+            <p>{content.period}</p>
+            {content.video !== "" ? embedVideo : null}
+            {content.images.length > 0 ? content.images.map(image => screenshot(image.src, image.alt)) : null}
+            <p>{content.notes}</p>
+
         </div>
     );
 }
 
 function ArtEntry(props) {
     return (
-        <div>
-            <img src={require(`${props.content.src}`)} alt={props.content.title}/>
+        <div className="art-entry">
+            <img src={require(`${props.content.src}`)} alt={props.content.alt}/>
             <p>{props.content.title}</p>
         </div>
     );
@@ -20,8 +28,8 @@ function ArtEntry(props) {
 
 function Entry(props) {
     switch(props.type) {
-        case "Sites":
-            return <SiteEntry content={props.content}/>;
+        case "Projects":
+            return <ProjectEntry content={props.content}/>;
         case "Art":
             return <ArtEntry content={props.content}/>;
         default:
--- src/data.js
+++ src/data.js
@@ -1,8 +1,62 @@
 export const data = {
     pages: [
         {
-        title: "Games",
-        caption: "Some games I've developed from 2018 to 2022"
+        title: "Projects",
+        caption: "Some games I've developed from 2018 to 2022",
+        entries:[
+            {
+                title: "Romancelvania",
+                role: "Programmer",
+                period: "May 2021 ~ October 2022",
+                notes: "Designed and scripted attack behaviors for boss battles",
+                links: [
+                    {
+                        title: "Steam Page",
+                        url: "https://store.steampowered.com/app/1192810/Romancelvania/"
+                    }
+                ], 
+                technologies: [
+                    "Unreal Engine 4",
+                    "Tortoise SVN"
+                ],
+                tags: [
+                    "Games"
+                ],
+                video: "https://www.youtube.com/watch?v=299SVu_VObs",
+                images: [
+                    {
+                        src: "./images/art/concept-backpacks-2.jpg",
+                        alt: "Screenie"
+                    }
+                ],
+            },
+            {
+                title: "The Aquatic Messenger",
+                role: "Programmer",
+                period: "September 2020 ~ May 2022",
+                notes: "Designed and scripted attack behaviors for boss battles",
+                links: [
+                    {
+                        title: "Steam Page",
+                        url: "https://store.steampowered.com/app/1192810/Romancelvania/"
+                    }
+                ], 
+                technologies: [
+                    "Unreal Engine 4",
+                    "Tortoise SVN"
+                ],
+                tags: [
+                    "Games"
+                ],
+                video: "https://www.youtube.com/watch?v=299SVu_VObs",
+                images: [
+                    {
+                        src: "./images/art/concept-backpacks-2.jpg",
+                        alt: "Screenie"
+                    }
+                ],
+            }
+        ],
         },
         {
         title: "Sites",
@@ -13,44 +67,55 @@ export const data = {
         caption: "Some illustrations and models made over time",
         entries:[
             {
+                title: "Apple Store Interior",
                 src: "./images/art/concept-apple-shop.jpg",
-                title: "Apple store concept"
+                alt: "An environmental piece of apple store"
             },
             {
+                title: "Backpack concept",
                 src: "./images/art/concept-backpacks-2.jpg",
-                title: "Backpack concept"
+                alt: "Several design iterations of character themed backpacks"
+
             },
             {
+                title: "Backpack concept 2",
                 src: "./images/art/concept-backpacks-3.jpg",
-                title: "Backpack concept 2"
+                alt: "Several design iterations of character themed backpacks"
             },
             {
+                title: "Parrots",
                 src: "./images/art/concept-parrots.jpg",
-                title: "Parrots"
+                alt: "Several character design iterations of a middle aged parrot man"
             },
             {
+                title: "Skeleton character design",
                 src: "./images/art/concept-wellington.jpg",
-                title: "Skeleton character design"
+                alt: "Skeleton character design"
             },
             {
+                title: "Artblock",
                 src: "./images/art/illust-artblock.jpg",
-                title: "Artblock"
+                alt: "Person with an arm covered in prints holding a pencil"
             },
             {
+                title: "Cactus",
                 src: "./images/art/illust-cactus.jpg",
-                title: "Cactus"
+                alt: "A cactus with glasses looking far in the distance"
             },
             {
+                title: "Fries",
                 src: "./images/art/illust-fries.jpg",
-                title: "Fries"
+                alt: "An alligator man with a background of enlargened foreshortened french fries"
             },
             {
+                title: "Phone model",
                 src: "./images/art/model-phone.jpg",
-                title: "Phone model"
+                alt: "3d model of a rusty phone"
             },
             {
+                title: "Ramune model",
                 src: "./images/art/model-ramune.jpg",
-                title: "Ramune model"
+                alt: "3d model of a ramune bottle"
             },
         ]
         },
